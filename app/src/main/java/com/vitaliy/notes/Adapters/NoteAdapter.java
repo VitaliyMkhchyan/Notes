8ead1136f63d99411342a7794f41ee5a0fbf2a4a
VitaliyMkhchyan <vitaliy.mkhchyan@gmail.com>
2023-04-30 16:46:36 +0300

    Функция 'Избранное': пока не работает(

--- app/src/main/java/com/vitaliy/notes/Adapters/NoteAdapter.java
+++ app/src/main/java/com/vitaliy/notes/Adapters/NoteAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -21,6 +22,7 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder>{
     private List<Note> noteList;
     private final SetOnClickItem setOnClickItem;
     private final LayoutInflater inflater;
+    private static boolean flug = false;
 
     public NoteAdapter(List<Note> noteList, SetOnClickItem setOnClickItem, Context context) {
         this.noteList = noteList;
@@ -40,6 +42,7 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder>{
         return new ViewHolder(inflater.inflate(R.layout.item_note_model, parent, false));
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Note note = noteList.get(position);
@@ -47,6 +50,16 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder>{
         holder.title.setText(note.getTitle());
         holder.description.setText(note.getDescription());
         holder.date_of_creation.setText(note.getDate_of_creation());
+
+        holder.btn_star_note.setOnClickListener(view -> {
+            if (flug) {
+                holder.btn_star_note.setImageResource(R.drawable.ic_no_star);
+                flug = false;
+            } else {
+                holder.btn_star_note.setImageResource(R.drawable.ic_yes_star);
+                flug = true;
+            }
+        });
 
         // Обработка нажатия на элемент RecyclerView
         holder.itemView.setOnClickListener(view -> setOnClickItem.onClick(note));
@@ -60,12 +73,14 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder>{
     protected static class ViewHolder extends RecyclerView.ViewHolder {
         // Объекты модели
         final TextView title, description, date_of_creation;
+        final ImageButton btn_star_note;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.title_);
             description = itemView.findViewById(R.id.description);
             date_of_creation = itemView.findViewById(R.id.date);
+            btn_star_note = itemView.findViewById(R.id.btn_star_note);
         }
     }
 }
