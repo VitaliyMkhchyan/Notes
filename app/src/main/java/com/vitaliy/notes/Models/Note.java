9ba4f225a437db7f949a56f9f69e8238987989d9
VitaliyMkhchyan <vitaliy.mkhchyan@gmail.com>
2023-05-06 00:29:10 +0300

    Добавление метода setId() для установки идентификатора

--- app/src/main/java/com/vitaliy/notes/Models/Note.java
+++ app/src/main/java/com/vitaliy/notes/Models/Note.java
@@ -38,6 +38,10 @@ public class Note implements Serializable {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
